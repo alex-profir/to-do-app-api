eaa5a51d6ba5a9112b7cf770eceb2684bc122b3c
Alex Profir <alex_profir@yahoo.com>
2021-01-26 12:10:48 +0200

    updated ( after a long time ) readme.md also added register

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # to-do-app-api
+You first need to install the app using ### `npm install`
+
+After that you should run ### `npm run dev` to run de server and check the swagger ui on [http://localhost:4000]
+The swagger provides information about the api
--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -8,9 +8,27 @@ function router() {
             const uri = `mongodb+srv://admin:admin@cluster0-6d7le.mongodb.net/todo?retryWrites=true&w=majority`;
             const client = new MongoClient(uri, { useNewUrlParser: true });
             client.connect(async err => {
-                const collection = client.db("todo").collection("auth"); 
+                const collection = client.db("todo").collection("auth");
                 const response = await collection.insertOne({ email: "ion@admin.com", password: "admin" });
                 res.json(response);
+                client.close();
+            });
+        });
+    authRouter.route('/register')
+        .post((req, res) => {
+            const { email, password } = req.body;
+            const uri = `mongodb+srv://admin:admin@cluster0-6d7le.mongodb.net/todo?retryWrites=true&w=majority`;
+            const client = new MongoClient(uri, { useNewUrlParser: true });
+            client.connect(async err => {
+                const collection = client.db("todo").collection("auth");
+                const user = await collection.findOne({ email });
+                if (user) {
+                    res.status(400).json({ message: "Email already in use" });
+                    res.end();
+                } else {
+                    const response = await collection.insertOne({ email, password });
+                    res.json(response);
+                }
                 client.close();
             });
         });
